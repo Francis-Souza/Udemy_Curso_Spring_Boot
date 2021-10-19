30d2542c29e4a7b3b27cddb2abb9e66ecc311194
Francis-Souza <francisleisouza@gmail.com>
2021-10-18 23:02:21 -0300

    create project Demo Spring Boot

---
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot/CursoDemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.joinsolutions.curso_demo_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDemoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joinsolutions/curso_demo_spring_boot/CursoDemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joinsolutions.curso_demo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
