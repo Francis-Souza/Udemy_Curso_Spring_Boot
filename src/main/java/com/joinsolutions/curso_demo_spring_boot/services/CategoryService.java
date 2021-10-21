504eea1178b51e4696bdfc605e7d837695b0f464
Francis-Souza <francisleisouza@gmail.com>
2021-10-21 16:28:14 -0300

    category

---
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.joinsolutions.curso_demo_spring_boot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joinsolutions.curso_demo_spring_boot.entities.Category;
+import com.joinsolutions.curso_demo_spring_boot.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return categoryRepository.findAll();		
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+	
+}
