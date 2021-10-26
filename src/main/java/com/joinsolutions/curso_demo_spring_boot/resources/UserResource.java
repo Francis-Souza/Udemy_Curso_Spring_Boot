4e4f929d02d829bcf3e5e9e8127f2fe971d2b22e
Francis-Souza <francisleisouza@gmail.com>
2021-10-26 01:47:01 -0300

    Update user

--- src/main/java/com/joinsolutions/curso_demo_spring_boot/resources/UserResource.java
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot/resources/UserResource.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joinsolutions.curso_demo_spring_boot.entities.User;
-import com.joinsolutions.curso_demo_spring_boot.repositories.UserRepository;
 import com.joinsolutions.curso_demo_spring_boot.services.UserService;
 
 @RestController
