3d5098a033c3574e6dc463dbd5722bbbca06da34
Francis-Souza <francisleisouza@gmail.com>
2021-10-27 21:11:17 -0300

    Insert,update and delete Product

--- src/main/java/com/joinsolutions/curso_demo_spring_boot/resources/ProductResource.java
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package com.joinsolutions.curso_demo_spring_boot.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joinsolutions.curso_demo_spring_boot.entities.Product;
 import com.joinsolutions.curso_demo_spring_boot.services.ProductService;
@@ -33,6 +39,36 @@ public class ProductResource {
 		
 		Product product = productService.findById(id);
 		return ResponseEntity.ok().body(product);
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product obj) {
+		obj = productService.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		productService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product obj) {
+		obj = productService.update(id, obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(obj);
+
 	}
 	
 
--- src/main/java/com/joinsolutions/curso_demo_spring_boot/services/ProductService.java
+++ src/main/java/com/joinsolutions/curso_demo_spring_boot/services/ProductService.java
@@ -3,11 +3,17 @@ package com.joinsolutions.curso_demo_spring_boot.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.joinsolutions.curso_demo_spring_boot.entities.Product;
 import com.joinsolutions.curso_demo_spring_boot.repositories.ProductRepository;
+import com.joinsolutions.curso_demo_spring_boot.services.exceptions.DatabaseException;
+import com.joinsolutions.curso_demo_spring_boot.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -22,7 +28,40 @@ public class ProductService {
 	public Product findById(Long id) {
 		Optional<Product> obj = productRepository.findById(id);
 		return obj.get();
+	}	
+	
+	public Product insert(Product obj) {
+		return productRepository.save(obj);
 	}
 	
+	public void deleteById(Long id) {
+		try {
+			productRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}	
+	
+	@SuppressWarnings("deprecation")
+	public Product update(Long id, Product obj) {
+		try {			
+			Product product = productRepository.getOne(id);
+			updateData(product, obj);
+			return productRepository.save(obj);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product product, Product obj) {
+		
+		product.setName(obj.getName());
+		product.setDescription(obj.getDescription());
+		product.setPrice(obj.getPrice());
+		product.setImgUrl(obj.getImgUrl());
+		
+	}
 
 }
